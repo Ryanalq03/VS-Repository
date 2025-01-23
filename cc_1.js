e729f46048c8a5bf72a335a089f2d132a1272350
Ryan Alqasemi <alqasemir@usf.edu>
2025-01-23 12:53:07 -0500

    Task 1 - Employee Information

---
+++ cc_1.js
@@ -0,0 +1,12 @@
+let employeeName = "John Snow";
+const employeeID = 123;
+var isActive = true;
+
+console.log(employeeName); // "John Snow"
+console.log(typeof employeeName); //"string"
+
+console.log(employeeID); //123
+console.log(typeof employeeID); //"number"
+
+console.log(isActive); //true
+console.log(typeof isActive); //"boolean"
