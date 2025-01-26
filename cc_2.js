2af98f8e35d919e5c90c97177227f8d60ac65847
Ryan Alqasemi <alqasemir@usf.edu>
2025-01-26 15:20:43 -0500

    Task 2 - Student Scores

--- cc_2.js
+++ cc_2.js
@@ -4,3 +4,10 @@ products.push("Mouse");
 products.pop();
 console.log("Updated product list:", products);
 
+//Task 2
+let scores = [70, 65, 80, 75, 85];
+scores[1] = 90;
+let sum = scores.reduce((total, score) => total + score, 0);
+let average = sum/ scores.length;
+console.log("Updated Scores:");
+console.log("Average Score:");
