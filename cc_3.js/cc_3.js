4241e08a0885db4656c198a85b0b7e3d63236ead
Ryan Alqasemi <alqasemir@usf.edu>
2025-02-02 14:53:48 -0500

    Task 2 - Modifying Customer Orders

--- cc_3.js/cc_3.js
+++ cc_3.js/cc_3.js
@@ -1,16 +1,36 @@
 //Task 1: Product Price Management 
 
 //Declaring Array
-let prices = [100,200,300,400,500]
+let prices = [100,200,300,400,500];
 
 //Adding new price
-prices.push(600)
+prices.push(600);
 
 //Removed first item
-prices.shift()
+prices.shift();
 
 //Update list to console
-console.log("Updated Prices:", prices)
+console.log("Updated Prices:", prices);
+
+//Task 2: Modifying Customer Order
+
+//Delare an array
+let orders = [10,20,30,40,50];
+
+//Increase third quantity by 5
+orders[2] += 5;
+
+//Logging new array
+console.log("Updated Orders:", orders);
+
+//Calculating Total orders
+let total_orders = orders.reduce((total, order) => total + order, 0);
+
+//Log total orders to console
+console.log("Total Orders:", total_orders);
+
+
+
 
 
 
