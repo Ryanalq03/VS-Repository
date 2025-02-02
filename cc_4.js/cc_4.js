692b9ba893d9bdb452e7c56b57daad2feb23beb4
Ryan Alqasemi <alqasemir@usf.edu>
2025-02-02 15:35:55 -0500

    Task 1 - Customer Discounts

---
+++ cc_4.js/cc_4.js
@@ -0,0 +1,12 @@
+//Task 1: Customer Discounts
+
+//Declaring a variable
+let purchaseAmount = 500;
+
+//10% discount applied
+if (purchaseAmount > 100) {
+    FinalAmount = purchaseAmount * 0.9;
+}
+
+//Loged the final amount to console
+console.log(`Final amount after discount: $${finalAmount}`);
