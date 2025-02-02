0ebd890cfb2c9e720a995617bd7ca002b523fe05
Ryan Alqasemi <alqasemir@usf.edu>
2025-02-02 14:39:32 -0500

    Task 1 - Product Price Management

---
+++ cc_3.js/cc_3.js
@@ -0,0 +1,18 @@
+//Task 1: Product Price Management 
+
+//Declaring Array
+let prices = [100,200,300,400,500]
+
+//Adding new price
+prices.push(600)
+
+//Removed first item
+prices.shift()
+
+//Update list to console
+console.log("Updated Prices:", prices)
+
+
+
+
+
