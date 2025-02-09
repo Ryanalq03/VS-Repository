f42eb987aa1a272f924ca77a92b848f7a55c85d0
Ryan Alqasemi <alqasemir@usf.edu>
2025-02-09 17:29:01 -0500

    Task 1 - Business Profit Calculation

---
+++ cc_6.js/cc_6.js
@@ -0,0 +1,10 @@
+//Task 1: Function Delaration
+
+//Created formula to calculate profit
+function calculateProfit(costPrice, sellingPrice, unitsSold) {
+    let profit = (sellingPrice - costPrice) * unitsSold;
+    console.log(`Total Profit: $${profit}`);
+}
+
+calculateProfit(20, 30, 100);
+calculateProfit(50, 70, 200);
