75612fb03bb3661c4bf6e86737d529d54be4a282
Ryan Alqasemi <alqasemir@usf.edu>
2025-02-09 16:57:49 -0500

    Task 2 - Order Details

--- cc_5.js/cc_5.js
+++ cc_5.js/cc_5.js
@@ -6,3 +6,28 @@ let customer = {
     age: 35,
     email: "John.doe@email.com"
 };
+
+//Use a for in loop to iterate through object properties
+for (let Property in customer) {
+    //logging each property to console
+    console.log(`${property}: ${customer[property]}`)
+};
+
+//Task 2 Object Methods
+
+//Declaring the object's order details
+let order = {
+    orderID: 12345,
+    totalAmount: 150,
+    status: "Processing",
+
+    //Add method to log order details to the console
+    displayOrder: function() {
+        console.log("Order ID:", this.orderID),
+        console.log("Total Amount:", this.totalAmount),
+        console.log("Status:", this.status) 
+    }
+};
+
+//Calling method to display details
+order.displayOrder();
