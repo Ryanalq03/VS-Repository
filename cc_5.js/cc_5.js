65ff9b139d889ed2f0513a17fe37b1f5cfcd3bc4
Ryan Alqasemi <alqasemir@usf.edu>
2025-02-09 16:53:14 -0500

    Task 1 - Customer Profile.

---
+++ cc_5.js/cc_5.js
@@ -0,0 +1,8 @@
+//Task 1: Object Properties - Customer Profile
+
+//Declaring an object containing customer info
+let customer = {
+    name: "Jane Doe",
+    age: 35,
+    email: "John.doe@email.com"
+};
