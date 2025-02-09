f403c152f0ec15f202996ae4c3b60dfc1f6a902a
Ryan Alqasemi <alqasemir@usf.edu>
2025-02-09 17:32:00 -0500

    Task 3 - Employee Bonus Calculation

--- cc_6.js/cc_6.js
+++ cc_6.js/cc_6.js
@@ -18,5 +18,20 @@ function calculateSalesTax(amount, taxRate) {
 
 };
 
-calculateSalesTax(100, 0.07); // Expected output: "Sales Tax: $7"
-calculateSalesTax(500, 0.1);  // Expected output: "Sales Tax: $50";
+calculateSalesTax(100, 0.07); 
+calculateSalesTax(500, 0.1);  
+
+//Task 3: Arrow Function
+
+//Calculates bonus based on performance
+const calculateBonus = (salary, performanceRating) => 
+    performanceRating === "Excellent" ? salary * 0.2 :
+    performanceRating === "Good" ? salary * 0.1 :
+    performanceRating === "Average" ? salary * 0.05 :
+    0;
+
+console.log(`Bonus: $${calculateBonus(5000, "Excellent")}`); 
+console.log(`Bonus: $${calculateBonus(7000, "Good")}`); 
+
+calculateBonus(5000, "Excellent"); 
+calculateBonus(7000, "Good");
