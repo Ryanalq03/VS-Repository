f7cad5aa2658ce236a00d7d10e7c1c23f723f148
Ryan Alqasemi <alqasemir@usf.edu>
2025-02-16 20:19:07 -0500

    Task 2 - Product Price After Discount

--- cc_8.js/cc_8.js
+++ cc_8.js/cc_8.js
@@ -12,3 +12,17 @@ function calculateSalary(baseSalary, bonus, taxRate) {
 //Test Cases
 calculateSalary(5000, 500, 0.1);
 calculateSalary(7000, 1000, 0.15);
+
+//Task 2: Product price after discount
+
+//Declaring function to find final price
+const calculateDiscount = function(price, discountRate) {
+    const finalPrice = price - (price * discountRate);
+
+//Logging final price to console
+console.log(`Final price: $${finalPrice.toFixed(2)}`);
+};
+
+//Test Cases
+calculateDiscount(100, 0.2);
+calculateDiscount(250, 0.15);
