6f64a2fe0bb7775653d0829a908e08f031310fd8
Ryan Alqasemi <alqasemir@usf.edu>
2025-02-16 19:49:09 -0500

    Task 2 - Employee Hourly Wage Calculation

--- cc_7.js/cc_7.js
+++ cc_7.js/cc_7.js
@@ -14,3 +14,16 @@ console.log(`Total invoice: $${total.toFixed(2)}`);
 //Testing data
 calculateInvoice(100, 0.08, 5); //Expected output: $103.00
 calculateInvoice(500, 0.1, 20); //Expected output: $530.00
+
+//Task 2: Function Expression
+
+//Function that calculates hourly wage
+const calculateHourlyWage = function(salary, hoursPerWeek) {
+    let hourlyWage = salary / (hoursPerWeek * 52);
+//Logging results to console.
+console.log(`Hourly Wage: $${hourlyWage.toFixed(2)}`);
+}
+
+//Testing data
+calculateHourlyWage(52000, 40); //Expected output: $25.00
+calculateHourlyWage(75000, 35); //Expected output: $41.21
